3b5bbcf1ccb8242ef17accff422db2b94e20bf5c
g-ames <132623120+g-ames@users.noreply.github.com>
2025-09-17 02:23:35 +0000

    epic

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ let initTime = +(new Date());
 // ------------------------
 // Game
 // ------------------------
+let lastFrameTime = initTime;
 function tick() {
+    let frameTime = +(new Date());
+    let deltatime = frameTime - lastFrameTime;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = "rgb(200 0 0)";
@@ -37,6 +40,7 @@ function tick() {
     ctx.fillStyle = "rgb(0 0 255)";
     ctx.fillRect(mousePosition.x + Math.sin(), mousePosition.y + 30, 50, 50);
     
+    lastFrameTime = frameTime;
     requestAnimationFrame(tick);
 }
 
