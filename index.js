1758dfac108f73c5cb15329e407b1f60ff8c8b52
g-ames <132623120+g-ames@users.noreply.github.com>
2025-09-17 02:22:14 +0000

    Zilgon-e init

---
+++ index.js
@@ -0,0 +1,43 @@
+// ------------------------
+// Setup
+// ------------------------
+const ctx = canvas.getContext("2d");
+
+function fixCanvasSize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", fixCanvasSize);
+fixCanvasSize();
+
+var mousePosition = {
+    x: 0,
+    y: 0
+};
+
+window.addEventListener("mousemove", (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    mousePosition = {x, y};
+});
+
+let initTime = +(new Date());
+
+// ------------------------
+// Game
+// ------------------------
+function tick() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "rgb(200 0 0)";
+    ctx.fillRect(mousePosition.x, mousePosition.y, 50, 50);
+
+    ctx.fillStyle = "rgb(0 0 255)";
+    ctx.fillRect(mousePosition.x + Math.sin(), mousePosition.y + 30, 50, 50);
+    
+    requestAnimationFrame(tick);
+}
+
+requestAnimationFrame(tick);
