4ebb1d18abf8336d7cefc64c9ce681a769a97b45
g-ames <132623120+g-ames@users.noreply.github.com>
2025-09-17 02:38:10 +0000

    asdasd

--- index.js
+++ index.js
@@ -29,17 +29,24 @@ let initTime = +(new Date());
 // Game
 // ------------------------
 let lastFrameTime = initTime;
+let mode = "intro";
 function tick() {
     let frameTime = +(new Date());
     let deltatime = frameTime - lastFrameTime;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = "rgb(200 0 0)";
-    ctx.fillRect(mousePosition.x, mousePosition.y, 50, 50);
-
-    ctx.fillStyle = "rgb(0 0 255)";
-    ctx.fillRect(mousePosition.x + Math.sin(), mousePosition.y + 30, 50, 50);
     
+    switch(mode) {
+        case "intro":
+
+            break;
+        default:
+            ctx.font = "48px serif";
+            ctx.fillStyle = "white";
+            ctx.fillText("g engine no state", 80, 80, canvas.width);
+            break;
+    } 
+
     lastFrameTime = frameTime;
     requestAnimationFrame(tick);
 }
